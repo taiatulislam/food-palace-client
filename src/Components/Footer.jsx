35d1baa15d4914d58782ea6440f99bf14090135e
taiatulislam <taiatulislamapon@gmail.com>
2025-06-28 23:08:25 +0600

    fixed footer and blog responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,11 @@
-import logoLight from "../assets/images/logo/logo-light.jpg";
+import { FaMapLocationDot } from "react-icons/fa6";
 import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
 import { LuMailOpen } from "react-icons/lu";
-import { FaMapLocationDot } from "react-icons/fa6";
+import logoLight from "../assets/images/logo/logo-light.jpg";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div
       className="mt-10"
@@ -13,8 +15,8 @@ const Footer = () => {
       }}
     >
       <footer className="footer p-5 md:p-7 text-primary-content max-w-7xl mx-auto gap-4 py-20">
-        <div className="flex gap-5">
-          <div className="text-white w-2/3">
+        <div className="flex gap-5 justify-between w-full">
+          <div className="text-white">
             <div className="flex items-center gap-2 md:gap-5 mb-5">
               <img
                 src={logoLight}
@@ -22,44 +24,42 @@ const Footer = () => {
                 className="w-[60px] md:w-[100px] rounded-lg"
               />
               <div>
-                <h3 className="text-lg md:text-2xl">Food Palace</h3>
-                <p className="text-[#dc3545] text-base md:text-lg font-medium">
+                <h3 className="text-lg md:text-3xl">Food Palace</h3>
+                <p className="text-[#dc3545] md:text-lg font-medium">
                   Food & Restaurant
                 </p>
               </div>
             </div>
-            <p className="text-base md:text-lg text-justify">
+            <p className="text-base md:text-lg text-justify max-w-lg">
               All produced by using ingredients and serving up delicious food,
               every time. Including pizza and pasta dishes, as well as fresh
               salads, burger, steaks, risottos and indulgent desserts.
             </p>
 
             <div>
-              <h2 className="text-lg font-bold mt-10">Newsletter</h2>
+              <h2 className="text-lg mt-10">Newsletter</h2>
               <hr className="w-[90px] border-2 border-[#ffc107] rounded-lg" />
-              <p className="font-medium my-2">
-                Subscribe and Get Recent News and Updates
-              </p>
-              <div className="flex flex-col md:flex-row gap-3">
+              <p className="my-2">Subscribe and Get Recent News and Updates</p>
+              <div className="flex flex-col md:flex-row md:justify-between gap-3">
                 <input
                   type="text"
                   placeholder="Enter Your Email Address ..."
-                  className="w-full md:w-[50%] bg-transparent border-2 border-gray-500 rounded-lg p-2 md:pl-5 text-xs md:text-base"
+                  className="w-full md:w-[80%] bg-transparent border-2 border-gray-500 rounded-lg p-2 md:pl-5 text-xs md:text-base"
                 />
-                <button className="bg-[#dc3545] p-3 font-semibold rounded-lg">
+                <button className="bg-[#dc3545] p-3 font-medium rounded-lg">
                   Subscribe
                 </button>
               </div>
             </div>
           </div>
 
-          <div className="text-white w-1/2 md:w-1/3">
-            <h2 className="text-lg font-bold">Contact Info</h2>
+          <div className="text-white">
+            <h2 className="text-lg">Contact Info</h2>
             <hr className="w-[100px] border-2 border-[#ffc107] rounded-lg" />
-            <p className="py-2 md:py-4">Mon - Sat: 1.00 p.m to 3.00 p.m</p>
+            <p className="py-1.5 md:py-3">Mon - Sat: 1.00 p.m to 3.00 p.m</p>
             <p className="">Sun: 12.30 pm to 3.30 p.m</p>
-            <p className="py-2 md:py-4">Festival: Open Full Day</p>
-            <div className="mt-3 md:mt-0">
+            <p className="py-1.5 md:py-3">Festival: Open Full Day</p>
+            <div className="mt-3">
               {/* Mobile */}
               <div className="flex items-center gap-2">
                 <p>
@@ -97,7 +97,7 @@ const Footer = () => {
         </div>
       </footer>
       <p className="text-white bg-black text-center py-4">
-        Copyright © 2023 - All right reserved ||{" "}
+        Copyright © {currentYear} - All right reserved ||{" "}
         <span className="text-[#dc3545]">Food Palace</span>
       </p>
     </div>
